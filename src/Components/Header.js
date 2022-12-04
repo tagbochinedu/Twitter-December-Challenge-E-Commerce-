657d34984e748bb8650a42f676c5f5c05a8c4c3a
johntagbo <johntagbo2@yahoo.com>
2022-12-04 07:15:09 +0100

    responsive design incomplete-header in progress

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,39 +1,68 @@
-import React from "react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
+  const [sidebar, setSidebar] = useState(false);
   return (
-    <div className="flex justify-between items-baseline pl-48 pt-14 pr-32 text-lbl">
+    <div className="flex justify-between items-center md:items-baseline xl:pl-48 xl:pt-14 xl:pr-32 lg:px-36 lg:pt-10 md:px-24 px-5 pt-8 text-lbl">
       <div>
-        <h2 className="font-stix text-3xl font-bold">ARTSY.</h2>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-5 h-5 md:w-10 md:h-10 block lg:hidden cursor-pointer"
+          onClick={() => {
+            setSidebar(!sidebar);
+          }}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+
+        <h2 className="font-stix text-3xl font-bold hidden lg:block">ARTSY.</h2>
       </div>
-      <div className="font-satoshi font-medium text-2xl">
+      <div className="font-satoshi font-medium text-2xl hidden lg:block">
         <NavLink className="px-6">Home</NavLink>
         <NavLink className="px-6">MarketPlace</NavLink>
         <NavLink className="px-6">Auctions</NavLink>
         <NavLink className="px-6">Drop</NavLink>
       </div>
+      <div className="block lg:hidden">
+        <h2 className="font-stix text-2xl md:text-3xl font-bold">ARTSY.</h2>
+      </div>
       <div className="flex items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 576 512"
-          className="w-10 h-10 mx-6"
-        >
-          <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-          className="w-10 h-10 mx-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-5 h-5 md:w-10 md:h-10 mr-2 md:mr-6"
         >
           <path
-            d="M224 0c-17.7 0-32 14.3-32 32V51.2C119 66 64 130.6 64
-          208v18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3
-          34.4S19.4 416 32 416H416c12.6 0 24-7.4
-          29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384
-          226.8V208c0-77.4-55-142-128-156.8V32c0-17.7-14.3-32-32-32zm45.3
-          493.3c12-12 18.7-28.3 18.7-45.3H224 160c0 17 6.7 33.3 18.7 45.3s28.3
-          18.7 45.3 18.7s33.3-6.7 45.3-18.7z"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-5 h-5 md:w-10 md:h-10 ml-2 md:ml-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
           />
         </svg>
       </div>
