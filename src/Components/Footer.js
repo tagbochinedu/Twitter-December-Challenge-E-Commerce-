85aeaf04725e79b4714fe313ef79fa104463b389
johntagbo <johntagbo2@yahoo.com>
2022-12-11 17:18:12 +0100

    resuming work

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ import svg2 from "../Resources/Vector2.svg";
 
 const Footer = () => {
   return (
-    <footer className=" mx-5 pt-8 lg:pt-16">
+    <footer className="xl:pl-48 xl:pr-32 lg:px-36 lg:pt-10 md:px-24 px-5 pt-8 lg:pt-16">
       <div className="flex justify-between mb-[91px]">
         <div className="hidden lg:flex justify-center items-center w-4/12">
           <h2 className="font-stix text-3xl font-bold">ARTSY.</h2>
@@ -41,7 +41,9 @@ const Footer = () => {
           </ul>
         </div>
         <div className="w-full lg:w-4/12">
-          <h3 className="text-xl font-poppins mb-4 block lg:hidden">Reach Us.</h3>
+          <h3 className="text-xl font-poppins mb-4 block lg:hidden">
+            Reach Us.
+          </h3>
           <div className="flex items-center mb-8">
             <img src={svg} alt="svg" className="w-8 mr-2 lg:mr-4" />
             <a
